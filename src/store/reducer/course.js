22c5d6cbc001d34954063d21d11700eb0f950749
Eduardo Vilke <eduardo.vilke@unochapeco.edu.br>
2021-07-31 17:19:59 -0300

    first commit

---
+++ src/store/reducer/course.js
@@ -0,0 +1,37 @@
+const INITIAL_STATE = {
+  activeLesson: {},
+  activeModule: {},
+  modules: [
+    { 
+      id: 1, 
+      title: 'Iniciando com react', 
+      lessons: [
+        { id: 1, title: 'Primeira aula' },
+        { id: 2, title: 'Segunda aula' },
+        { id: 3, title: 'Terceira aula' },
+        { id: 4, title: 'Quarta aula' }
+      ]
+    },
+    {
+      id: 2,
+      title: 'Aprendendo Redux',
+      lessons: [
+        { id: 1, title: 'Primeira aula' },
+        { id: 2, title: 'Segunda aula' },
+        { id: 3, title: 'Terceira aula' },
+        { id: 4, title: 'Quarta aula' }
+      ]
+    }    
+  ]
+}
+  
+export default function course(state = INITIAL_STATE, action) {
+  if(action.type === 'TOGGLE_LESSON') {
+    return {
+      ...state,
+      activeModule: action.module,
+      activeLesson: action.lesson,
+    }
+  }
+  return state;
+}
